004c249b487c33edf3c4ae6d0d1a501c8eb85c31
Rinat <test@mail.ru>
2024-02-19 00:43:17 +0600

    добавил дз класс объекты и equels

---
+++ src/Lesson27/HomeEx/Song.java
@@ -0,0 +1,29 @@
+package Lesson27.HomeEx;
+
+public class Song {
+
+        private final String title;
+        private final String artist;
+        private final String songwriter;
+
+        // переопределите метод equals(Object)
+
+
+    @Override
+    public String toString() {
+        return "Song{" +
+                "title='" + title + '\'' +
+                ", artist='" + artist + '\'' +
+                ", songwriter='" + songwriter + '\'' +
+                '}';
+    }
+
+    public Song(String title, String artist, String songwriter) {
+        this.title = title;
+        this.artist = artist;
+        this.songwriter = songwriter;
+
+
+    }
+}
+

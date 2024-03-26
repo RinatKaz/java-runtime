1d2860f9b973e4f5e8afd6e72f2ef1995ed41019
Rinat <test@mail.ru>
2024-03-26 13:22:22 +0500

    ищет зеркальные цифры, но преобразует в строку (уже после того как сделал задачу, прочитал что нужно непреобазовывая в строку сделать.

---
+++ src/Lesson36_binarniiPoisk/Home_2/MainForNumbers.java
@@ -0,0 +1,35 @@
+package Lesson36_binarniiPoisk.Home_2;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class MainForNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        HashSet<NumberUser> hSet1 = new HashSet<>();
+        System.out.println("Введите число: ");
+        while (true) {
+            String number = scanner.nextLine();
+            int count;//количество символов
+            count = number.length();
+            int x = 1;
+
+            for (int i = 0; i <= Math.ceil(number.toCharArray().length / 2); i++) {
+                if (number.charAt(i) == number.charAt(count - (i + 1))) {
+                    x = 1;
+                } else {
+                    x = 2;
+                    break;
+                }
+            }
+            if (x == 1) {
+                System.out.println("число подходит");
+            } else {
+                System.out.println("число не подходит");
+            }
+            System.out.println();
+            System.out.println("Введите число: ");
+        }
+
+    }
+}
